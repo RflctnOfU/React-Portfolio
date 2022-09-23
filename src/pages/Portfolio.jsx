974f2d6ea2c49a26af20c5ea90444ec5c4cd3925
Kris Marshall <rflctnofu@hotmail.com>
2022-09-23 12:38:21 -0400

    adjusted grid gap in portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import ProjectData from '../projects/ProjectData'
-import Card from '../components/Card'
+import React from "react";
+import ProjectData from "../projects/ProjectData";
+import Card from "../components/Card";
 
 const Portfolio = () => {
-    const projects = ProjectData
-    return (
-        <div className='my-16 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-16'>
-            {projects.map((project) =>
-                <Card id={project.id} image={project.image} description={project.description} title={project.title} live={project.live} github={project.github} />
-            )}
-        </div>
-    )
-}
+  const projects = ProjectData;
+  return (
+    <div className="my-16 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-24">
+      {projects.map((project) => (
+        <Card
+          id={project.id}
+          image={project.image}
+          description={project.description}
+          title={project.title}
+          live={project.live}
+          github={project.github}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
